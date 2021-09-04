af987626fcf400c45e2c6fa96dd6a4c0312a8388
AmeerLalaDeveloper <laklaw100@gmail.com>
2021-09-04 10:39:02 +0300

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var header=document.querySelector('header');
+var span=document.querySelector('.logo-span')
+window.onscroll=function () {
+
+    var top=window.scrollY;
+    if(top>=200){
+        
+header.classList.add('active');
+span.style.color="#fff";
+
+    }
+    else{
+
+        header.classList.remove('active');
+        span.style.color="rgb(167, 79, 93)";
+
+    }
+}
